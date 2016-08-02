d9130628d2af92bf04c4f4fcb057ae5e0ca05381
Adrian McLaughlin <subwoofer359@gmail.com>
2016-08-02 09:37:18 +0100

    remove magic numbers from download.js

--- routes/download.js
+++ routes/download.js
@@ -1,11 +1,14 @@
 var downloader = require('../lib/downloader');
+const HTTP_INTERNAL_ERROR = 500,
+      HTTP_BAD_REQUEST = 400,
+      ERROR_NO_URL = 'No url given\n';
 exports.download = function (req, res) {
 	if(req.body.url) {
 		res.type('application/text');
 		downloader.downloader(req.body.url, function (err, message) {
 			if(err) {
 				if(!res.finished) {
-					res.status(500);
+					res.status(HTTP_INTERNAL_ERROR);
 					res.send(err.message + "\n");
 					res.end();
 				}
@@ -16,8 +19,8 @@ exports.download = function (req, res) {
 		});
 		
 	} else {
-		res.status(400);
-		res.send('No url given\n');
+		res.status(HTTP_BAD_REQUEST);
+		res.send(ERROR_NO_URL);
 		res.end();
 	}
 	
